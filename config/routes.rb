24519d75e287897d37c4b26f6cf23bd3286b8a8a
Michelle  Ler Hsin Yee <MichelleLer@Michelles-MacBook-Pro.local>
2017-06-30 00:01:09 +0800

    add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'posts#index'
+  root 'posts#index', as: '/'
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
